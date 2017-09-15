5228e9b71b6fa6f36e68a9e19cef07ac8de0599f
Nidheesh <nidheeshpandey@gmail.com>
2017-09-15 21:33:49 +0530

    added input from file

---
+++ inputGrid.py
@@ -0,0 +1,6 @@
+
+def getGrid(puzzleFile) :
+	with open(puzzleFile,'r') as mypuzzle :
+		puzzle = mypuzzle.readlines()
+	puzzle = [list(map(int,i.replace('\n',"").split())) for i in puzzle]
+	return puzzle
